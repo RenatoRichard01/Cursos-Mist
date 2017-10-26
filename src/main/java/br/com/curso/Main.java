84a64317c7eeeae8abe032b98419a511560ea9ae
Renato Richard <renatorichard2012@gmail.com>
2017-10-26 17:50:11 -0200

    preparando o REST

--- src/main/java/br/com/curso/Main.java
+++ src/main/java/br/com/curso/Main.java
@@ -1,10 +1,12 @@
+package br.com.curso;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		SpringApplication.run(Main.class, args);
-	}
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Main.class, args);
+  }
 }
