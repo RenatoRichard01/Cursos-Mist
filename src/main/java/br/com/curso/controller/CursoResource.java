ea801b6bd8d45676c7af2a6dfe427906b2e217a1
Renato Richard <renatorichard2012@gmail.com>
2017-11-22 17:43:58 -0200

    atividade concluida. Passando por aprimoramento.

--- src/main/java/br/com/curso/controller/CursoResource.java
+++ src/main/java/br/com/curso/controller/CursoResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,10 @@ public class CursoResource {
     private CursosDao cursosD;
     
     @GetMapping
-    public List<Cursos> lista(){
+    public List<Cursos> pesquisar(String nome){
+    	if (nome != null) {
+    		return cursosD.pesquisarCursos(nome);
+    	}
     	return cursosD.findAll();
     }
     
@@ -40,6 +44,13 @@ public class CursoResource {
      cursosD.delete(id);
      return cursosD.findAll();
 	}
+	
+	@PutMapping
+	public List<Cursos> alterar(@RequestBody Cursos curso){
+		cursosD.save(curso);
+		return cursosD.findAll();
+	}
+	
 	
 	
 	/*@RequestMapping(value = "/", method = RequestMethod.PUT)
--- src/main/java/br/com/dao/CursosDao.java
+++ src/main/java/br/com/dao/CursosDao.java
@@ -1,6 +1,9 @@
 package br.com.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -8,5 +11,8 @@ import br.com.curso.controller.javabeans.Cursos;
 
 @Repository
  public interface CursosDao extends JpaRepository<Cursos, Long>{
+	Cursos findByNome(String nome);
 	
+	  @Query("from Cursos where nome like concat(?1, '%')")
+	    List<Cursos> pesquisarCursos(String nome);
 }
